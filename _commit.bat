0bcd960ff38f9c3daa4ff30b0d6d70fce0930b04
xdonotaskmex <momo.demirov@gmail.com>
2026-06-22 11:59:17 +0200

    Rename nav /home to [--home] with consistent bracket style

---
+++ _commit.bat
@@ -0,0 +1,4 @@
+@echo off
+"C:\Users\user\AppData\Local\GitHubDesktop\app-3.5.12\resources\app\git\cmd\git.exe" -C "c:\Users\user\OneDrive\Dokumenty\GitHub\Telegram-Bot" add -A
+"C:\Users\user\AppData\Local\GitHubDesktop\app-3.5.12\resources\app\git\cmd\git.exe" -C "c:\Users\user\OneDrive\Dokumenty\GitHub\Telegram-Bot" commit -m "Rename nav /home to [--home] with consistent bracket style"
+"C:\Users\user\AppData\Local\GitHubDesktop\app-3.5.12\resources\app\git\cmd\git.exe" -C "c:\Users\user\OneDrive\Dokumenty\GitHub\Telegram-Bot" push
