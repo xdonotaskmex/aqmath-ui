a87d45f5d623d36ae60bc7862f540ae40397a825
xdonotaskmex <momo.demirov@gmail.com>
2026-06-22 12:03:32 +0200

    Add *.bat to gitignore, cleanup temp scripts

--- _z.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-"C:\Users\user\AppData\Local\GitHubDesktop\app-3.5.12\resources\app\git\cmd\git.exe" -C "c:\Users\user\OneDrive\Dokumenty\GitHub\Telegram-Bot" add -A
-"C:\Users\user\AppData\Local\GitHubDesktop\app-3.5.12\resources\app\git\cmd\git.exe" -C "c:\Users\user\OneDrive\Dokumenty\GitHub\Telegram-Bot" commit -m "Remove temp scripts"
-"C:\Users\user\AppData\Local\GitHubDesktop\app-3.5.12\resources\app\git\cmd\git.exe" -C "c:\Users\user\OneDrive\Dokumenty\GitHub\Telegram-Bot" push
