1de76d50bc53446766019676d751985fe184ddd6
xdonotaskmex <momo.demirov@gmail.com>
2026-08-17 20:06:51 +0200

    style: fully unify all card bg+borders across app+landing (v=a0ee68aedc)

--- app-boot.js
+++ app-boot.js
@@ -333,7 +333,7 @@ var i18nResources = {};
 var i18nReady = false;
 
 function loadLocale(lang) {
-    return fetch('/locales/' + lang + '.json?v=a4a83531cf')
+    return fetch('/locales/' + lang + '.json?v=a0ee68aedc')
         .then(function (r) { return r.ok ? r.json() : null; })
         .catch(function () { return null; });
 }
