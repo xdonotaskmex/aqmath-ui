d3d6f4d08fcb86718b5f325ad1aad28894dca10f
xdonotaskmex <momo.demirov@gmail.com>
2026-08-17 20:01:57 +0200

    style: unify all card backgrounds to signal-card cyan tint (v=085574d49b)

--- app-boot.js
+++ app-boot.js
@@ -333,7 +333,7 @@ var i18nResources = {};
 var i18nReady = false;
 
 function loadLocale(lang) {
-    return fetch('/locales/' + lang + '.json?v=f61f0ba3d5')
+    return fetch('/locales/' + lang + '.json?v=085574d49b')
         .then(function (r) { return r.ok ? r.json() : null; })
         .catch(function () { return null; });
 }
