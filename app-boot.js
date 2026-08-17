74fb2350ca7a48476a9713f79f623dd29f2b5dd9
xdonotaskmex <momo.demirov@gmail.com>
2026-08-17 20:02:57 +0200

    style: What's New i18n keys + unified card backgrounds rebuild (v=a4a83531cf)

--- app-boot.js
+++ app-boot.js
@@ -333,7 +333,7 @@ var i18nResources = {};
 var i18nReady = false;
 
 function loadLocale(lang) {
-    return fetch('/locales/' + lang + '.json?v=085574d49b')
+    return fetch('/locales/' + lang + '.json?v=a4a83531cf')
         .then(function (r) { return r.ok ? r.json() : null; })
         .catch(function () { return null; });
 }
