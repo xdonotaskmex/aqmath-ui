1786b89550fd599ba86b2839afcb27db801fbd36
xdonotaskmex <momo.demirov@gmail.com>
2026-08-18 11:51:12 +0200

    chore: rebuild all pages with fresh asset stamp v=b0e4007d79

--- app-boot.js
+++ app-boot.js
@@ -333,7 +333,7 @@ var i18nResources = {};
 var i18nReady = false;
 
 function loadLocale(lang) {
-    return fetch('/locales/' + lang + '.json?v=8c56f56cc9')
+    return fetch('/locales/' + lang + '.json?v=b0e4007d79')
         .then(function (r) { return r.ok ? r.json() : null; })
         .catch(function () { return null; });
 }
